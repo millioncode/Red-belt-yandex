4526935fda21c65f69c0d0ed6659cab812de7d1b
Korben Dallas :) <34001730+1FoCuS@users.noreply.github.com>
2019-01-27 19:06:53 +0300

    Add files via upload

--- spotsmens/main.cpp
+++ spotsmens/main.cpp
@@ -1,44 +1,45 @@
-// не проходит 7/7 тест 0.99/0.8 - как ускорить хз
+/* 0.09/0.8 - если знаете как еще ускорить - пишите на scfocus@yandex.ru
+*/
 #include <iostream>
 #include <vector>
 #include <list>
 #include <utility>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 #include "test_runner.h"
 #include "profile.h"
 using namespace std;
 void Add(list <unsigned>& sports, const vector<pair<unsigned, unsigned>>& mans) {
+    // сохраняем в переменную конец листа - для ускорения на 0.8
+    auto it_end_sports = sports.end();
+    // вектор итерраторов на соответствующие элементы. Инициализирую концом листа - т.к.
+    //могут быть значения, которых нет в базе
+    vector <list <unsigned>::iterator> adress (100001, it_end_sports);
+    // константную сложность делаю.
     for(const auto& m : mans) {
-        auto it = sports.begin();
-        for (; it!=sports.end(); ++it) {
-            if (*it==m.second) break;
-        }
-        sports.insert(it, m.first);
+        auto it_adress = adress[m.second];
+        adress[m.first] = sports.insert(it_adress, m.first);
     }
 }
-
 void Test() {
-    {
-        LOG_DURATION("time ");
         list <unsigned> sportsmens;
-//        Add(sportsmens, {42,0});
-//        Add(sportsmens, {17, 42});
-//        Add(sportsmens, {13, 0});
-//        Add(sportsmens, {123, 42});
-//        Add(sportsmens, {5, 13});
         vector <pair<unsigned, unsigned>> aaa = {{42,0}, {17, 42}, {13, 0}, {123, 42}, {5, 13}};
         Add(sportsmens, aaa);
         list <unsigned> value {17, 123, 42, 5, 13};
         if (value == sportsmens) cerr << "OK" << endl;
-    }
 }
 int main()
 {
     Test();
+    // отключение даст выигрыш 0.04
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     size_t N;
     cin >> N;
     list <unsigned> sportsmens;
     vector < pair<unsigned, unsigned>> mans;
-    //mans.reserve(N);
     unsigned first, second;
     for(size_t i=0; i<N; ++i) {
         cin >> first >> second;
