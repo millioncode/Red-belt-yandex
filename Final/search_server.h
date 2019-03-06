be52945b315ed69e59fa05e25bd684654aeb6361
Korben Dallas :) <34001730+1FoCuS@users.noreply.github.com>
2019-03-06 21:49:11 +0300

    Add files via upload

--- Final/search_server.h
+++ Final/search_server.h
@@ -10,26 +10,45 @@
 using namespace std;
 
 class InvertedIndex {
-public:
-  void Add(const string& document);
-  list<size_t> Lookup(const string& word) const;
+    public:
+        void Add(const string& document);
+        list<size_t> Lookup(const string& word) const;
 
-  const string& GetDocument(size_t id) const {
-    return docs[id];
-  }
+        const string& GetDocument(size_t id) const {
+            return docs[id];
+        }
 
-private:
-  map<string, list<size_t>> index;
-  vector<string> docs;
+    private:
+        // ????
+        map<string, list<size_t>> index;
+        vector<string> docs;
 };
-
+/*
+ * непосредственно сам поиск
+ * */
 class SearchServer {
-public:
-  SearchServer() = default;
-  explicit SearchServer(istream& document_input);
-  void UpdateDocumentBase(istream& document_input);
-  void AddQueriesStream(istream& query_input, ostream& search_results_output);
+    public:
+        SearchServer() = default;
+        /*
+         * принимает поток документов
+         * один документ = одна строка
+         * */
+        explicit SearchServer(istream& document_input);
+        /*
+         * заменяет базу док-ов на новую
+*/
+        void UpdateDocumentBase(istream& document_input);
+        /*
+         * метод непосредственного поиска
+         * результат = максимум 5 док-ов
+         * [текст запроса]: {docid: <значение>, hitcount: <значение>}
+         * {docid: <значение>, hitcount: <значение>} ...,
+         *  где docid — идентификатор документа (см. ниже),
+         * а hitcount — значение метрики релевантности для данного документа
+         * (то есть суммарное количество вхождений всех слов запроса в данный документ).
+*/
+        void AddQueriesStream(istream& query_input, ostream& search_results_output);
 
-private:
-  InvertedIndex index;
+    private:
+        InvertedIndex index;
 };
